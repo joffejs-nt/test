4e0b8df556002cacfebdbb4587d0c3d1577bceea
Jackson Joffe <jackson.joffe@ninjatrader.com>
2025-12-03 17:46:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const dscc = window.dscc;
+
 function drawViz(data) {
   const el = document.getElementById("viz");
-  el.innerHTML = `<h2>${data.fields[0].name}: ${data.data[0][0]}</h2>`;
+  el.innerHTML = `
+    <h2>${data.fields[0].name}: ${data.data[0][0]}</h2>
+  `;
 }
 
-dscc.subscribeToData(drawViz, {transform: dscc.tableTransform});
+dscc.subscribeToData(drawViz, { transform: dscc.tableTransform });
